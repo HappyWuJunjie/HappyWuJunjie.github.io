b7f924fe0cc75f5d3ec41e37aedb84335d58a4c9
wjj <1911862126@qq.com>
2024-07-11 12:03:52 +0800

    Site updated: 2024-07-11 12:03:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/10/GPT-4提示工程大赛冠军/","2024/07/10/hello-world/"];function toRandomPost(){
+var posts=["2024/07/10/GPT-4提示工程大赛冠军/","2024/07/10/hello-world/","2024/07/11/三种常见的网格细化技术/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
